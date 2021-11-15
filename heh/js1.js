faf2e6492a538c247266492e2cee002fb8d128ec
BrockCotterill <92468632+BrockCotterill@users.noreply.github.com>
2021-11-15 12:28:37 +0100

    testuing

---
+++ heh/js1.js
@@ -0,0 +1,5 @@
+import * as j2 from './js2.mjs';  // val is "Hello";
+
+var l1 = new j2.laser("brock",18);
+
+console.log(l1.name);
---
+++ heh/js2.mjs
@@ -0,0 +1,6 @@
+export class laser{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+};
